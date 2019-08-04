f38782e526ce92e02efed11a9d45370cca1e4246
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-08-04 18:49:22 -0300

    Adicionar botões para roteamento utilizando Link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import HeaderStyle from "./HeaderStyle";
 
+import { Link } from "react-router-dom";
+
 class Header extends Component {
     render() {
         return (
             <header style={HeaderStyle.app}>
-                <h2>MasterTools</h2>
+                <Link to="/">MasterTools</Link>
+
+                <Link to="/contador">Contador</Link>
             </header>
         )
     }
